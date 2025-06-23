f42463806fcaf179b4850437ed4aa8dfa7dc8baf
Devesh Chandra <deveshru2712@gmail.com>
2025-06-23 17:29:30 +0530

    Implement the companion cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,44 @@
+import CompanionsCard from "@/components/CompanionsCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
 import React from "react";
 
 const Page = () => {
-  return <div></div>;
+  return (
+    <main>
+      <h1>Popular Companions</h1>
+      <section className="home-section">
+        <CompanionsCard
+          id={"123"}
+          name="Neura the Brainny Explorer"
+          topic="Neural network of brain"
+          subject="science"
+          duration={45}
+          color={"#ffda6e"}
+        />
+        <CompanionsCard
+          id="12wqer3"
+          name="Countsy the NUmber Wizard"
+          topic="Derivatives & Integrals"
+          subject="Maths"
+          duration={45}
+          color={"#e5d0ff"}
+        />
+        <CompanionsCard
+          id="12eqwr3"
+          name="Verba the vocal builder"
+          topic="Improve your vocab skills"
+          duration={30}
+          subject="English"
+          color={"#BDE7FF"}
+        />
+      </section>
+      <section className="home-section">
+        <CompanionsList />
+        <CTA />
+      </section>
+    </main>
+  );
 };
 
 export default Page;
---
+++ components/CompanionsList.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CompanionsList = () => {
+  return <div></div>;
+};
+
+export default CompanionsList;
