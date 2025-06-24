7d96332c9c51c7aaa08dc73ac894cd40a4e7e605
Devesh Chandra <deveshru2712@gmail.com>
2025-06-24 14:55:35 +0530

    implement billing

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,7 +1,12 @@
+import { PricingTable } from "@clerk/nextjs";
 import React from "react";
 
 const Subscription = () => {
-  return <div></div>;
+  return (
+    <main className="flex items-center justify-center">
+      <PricingTable />
+    </main>
+  );
 };
 
 export default Subscription;
