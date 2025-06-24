dd9467962d1938a11a0938042791d33e68d7dbe3
Devesh Chandra <deveshru2712@gmail.com>
2025-06-24 14:38:25 +0530

    implement clerk based auth

---
+++ env.d.ts
@@ -0,0 +1,9 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: string;
+    CLERK_SECRET_KEY: string;
+    NEXT_PUBLIC_CLERK_SIGN_IN_URL: string;
+    NEXT_PUBLIC_CLERK_SIGN_IN_FALLBACK_REDIRECT_URL: string;
+    NEXT_PUBLIC_CLERK_SIGN_UP_FALLBACK_REDIRECT_URL: string;
+  }
+}
